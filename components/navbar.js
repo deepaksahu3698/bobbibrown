5375cb063fd33571f5fd282ed733f21c0891ad51
BiswajitDas1802 <dasb1283@gmail.com>
2022-03-02 15:22:13 +0530

    homepage 80%

---
+++ components/navbar.js
@@ -0,0 +1,34 @@
+const nav =()=>{
+    return `
+    <div id="navbar">
+    <div class="left_nav">
+        <div>email signup</div>
+        <div><input type="text"></div>
+    </div>
+    <div class="mid_nav">
+        <div>
+            <h1>BOBBI  BROWN</h1>
+        </div>
+        <div>
+            <ul class="nav_ul">
+                <li>NEW</li>
+                <li>FAVORITES</li>
+                <li>MAKEUP</li>
+                <li>SKINCARE</li>
+                <li>MINI BAR</li>
+                <li>OFFERS</li>
+                <li>VIRTUAL SERVICES</li>
+                <li>DISCOVER</li>
+            </ul>
+        </div>
+    </div>
+    <div class="right_nav">
+        <p>loc</p>
+        <p>cart</p>
+        <p>pro</p>
+    </div>
+</div>
+    `
+}
+
+export default nav
---
+++ components/smallcrd.js
@@ -0,0 +1,33 @@
+const  smallcrd=()=>{
+    return`
+    <div>
+    <img src="../bobbiebrown/chatlive.png" >
+    <h4>LIVE CHAT</h4>
+    <p>Chat with a makeup artist to get product recommendations.</p>
+    <p>CHAT NOW</p>
+</div>
+<div>
+    <img src="../bobbiebrown/freesample.png" >
+    <h4>SAMPLE ON EVERY PURCHASE</h4>
+    <p>Chat with a makeup artist to get product recommendations.</p>
+    <p>CHAT NOW</p>
+</div>
+<div>
+    <img src="../bobbiebrown/freeship.png" >
+    <h4>FREE SHIPPING</h4>
+    <p> Get your favorites delivered straight<br>
+        to your door - every shipment on us</p>
+    <p>CHAT NOW</p>
+</div>
+<div>
+    <img src="../bobbiebrown/videochat.png" >
+    <h4>VIRTUAL CONSULTATION</h4>
+    <p> Get personalized recommendation on<br>
+        our bestsellers with Virtual<br>
+        Artistry Consultations</p>
+    <p>CHAT NOW</p>
+</div>
+    `
+}
+
+export default smallcrd
